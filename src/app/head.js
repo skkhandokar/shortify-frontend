1cb0b4e09a91df09b2126ff36ea4dbb8da46f352
Khandokar Samadul Kabir <111075442+skkhandokar@users.noreply.github.com>
2025-05-23 00:22:04 +0600

    Update head.js

--- src/app/head.js
+++ src/app/head.js
@@ -4,10 +4,7 @@ export default function Head() {
     <>
       <title>Shortify</title>
       <meta name= "Shortify" content="Instantly shorten, manage, and share your links with ease. Simple, fast, and powerful URL shortening." />
-      <script
-        type="text/javascript"
-        src="//pl26716472.profitableratecpm.com/1c/c9/f5/1cc9f58c866698492f57b2a5845644dc.js"
-      ></script>
+      <script type='text/javascript' src='//pl26716472.profitableratecpm.com/1c/c9/f5/1cc9f58c866698492f57b2a5845644dc.js'></script>
     </>
   );
 }
