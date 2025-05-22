c37660c9400abac4a04d78df341e9239c69a7ea9
Khandokar Samadul Kabir <111075442+skkhandokar@users.noreply.github.com>
2025-05-22 23:38:16 +0600

    Create head.js

---
+++ src/app/head.js
@@ -0,0 +1,13 @@
+
+export default function Head() {
+  return (
+    <>
+      <title>Shortify</title>
+      <meta name= "Shortify" content="Instantly shorten, manage, and share your links with ease. Simple, fast, and powerful URL shortening." />
+      <script
+        type="text/javascript"
+        src="//pl26716472.profitableratecpm.com/1c/c9/f5/1cc9f58c866698492f57b2a5845644dc.js"
+      ></script>
+    </>
+  );
+}
